1093e5d0474e8402948941c2a9ea08acc56e6b62
NicksPatties <nickspatties@gmail.com>
2024-06-04 22:16:36 -0700

    Remove unused files. Added file handling to scraper. Created a tsv with the scraper.

--- index.ts
+++ index.ts
@@ -3,34 +3,31 @@ import { chromium } from 'playwright';
 
 const log = (...strings: string[]) => console.log("[lb-scrape]", ...strings)
 
-let file : fs.FileHandle;
+const fileName = './bizkit-data.tsv'
+let file: fs.FileHandle;
 
 // Opening the file
 try {
-  file = await fs.open('./bizkit-data.csv', "r+")
+  file = await fs.open(fileName, "a")
 } catch (e) {
   console.error('Failed to open the data file. Exiting.')
   process.exit(1)
 }
-
-await file.write("fart\n")
-await file.write("fart again\n")
-await file.close()
 
 // Running the scraper
 const browser = await chromium.launch();
 const context = await browser.newContext();
 const page = await context.newPage()
 
-log("Opening Limp Bizkit's setlist page")
+log("Opening Limp Bizkit's setlist page...")
 
 await page.goto('https://www.setlist.fm/setlists/limp-bizkit-33d69c2d.html')
 
-log("Let's try and grab something!")
+log("Limp Bizkit's setlist page opened")
 
 const setLinks = await page.locator('a.summary.url').all()
 
-for (const setLink of setLinks){
+for (const setLink of setLinks) {
   const linkContent = await setLink.innerText()
   const setLinkUrl: string | null = await setLink.getAttribute('href')
 
@@ -39,11 +36,12 @@ for (const setLink of setLinks){
     continue;
   }
 
-  log(`Opening new page... `)
-  
+  log(`Opening page ${setLinkUrl}... `)
+
   const setLinkPage = await context.newPage()
+
   log(`Navigating to setlist set list page ${linkContent}`)
-  
+
   await setLinkPage.goto(`https://www.setlist.fm/${setLinkUrl.slice(3)}`)
   const setLinkPageTitle = await setLinkPage.title()
 
@@ -53,23 +51,36 @@ for (const setLink of setLinks){
 
   const songData = await setLinkPage.locator(".setlistParts.song").all()
 
+  let writtenSongCount = 0
+
   for (const song of songData) {
     const title = await song.locator(".songPart").innerText();
     const info = await song.locator(".infoPart").innerText();
     if (!title) {
-      log(`Whoops! Title does not exists die.`)
+      log(`Whoops! Song title doesn't exist for some reason... Continuing...`)
       continue;
     }
-    log(`  title: ${title}`)
-    log(`  info: ${!!info ? info : ""}`)
+    const row = `${title}\t${info}\t${linkContent}`
+    log(`Writing ${row} to ${fileName}`)
+    file.write(`${row}\n`)
+    writtenSongCount++;
+    log(`Writing ${row} to ${fileName} complete!`)
   }
 
-  log("Data retrieved!");
+  log(`Wrote ${writtenSongCount} ${writtenSongCount === 1 ? "song" : "songs"} to ${fileName}`);
 
-  log("Closing page", setLinkPageTitle)
+  log(`Closing page ${setLinkPageTitle}...`)
 
   await setLinkPage.close()
-  log("Page closed")
+  log(`Page ${setLinkPageTitle} closed`)
 }
 
+log(`Scraping completed! Closing browser...`)
 await browser.close();
+log(`Browser closed...`)
+
+log(`Closing ${fileName}...`)
+await file.close()
+log(`${fileName} closed...`)
+
+log(`All done! See ya! 🎸🎵`)
